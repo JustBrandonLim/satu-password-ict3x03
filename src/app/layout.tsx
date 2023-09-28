91f06256ecaac6e7e6d5d92e6a2a6a374fd86ce8
kevinthmm <kevinthmm@gmail.com>
2023-09-28 18:28:56 +0800

    merge fix more conflicts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
-import "./globals.css";
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-=======
 import './globals.css'
 import type { Metadata } from 'next'
 import { ThemeProvider } from "@/components/theme-provider"
->>>>>>> 0f1bc797ecbbc69113df4a55cb15463e2c2e1049
 
 export const metadata: Metadata = {
   title: "SatuPassword",
