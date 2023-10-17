816cbd7b9ffbbf66b8a577cf4e053361f426d47f
brandonut <heelabc@gmail.com>
2023-10-17 23:02:07 +0800

    Merged latest api backend

--- src/app/api/vault/retrieve/note/route.ts
+++ src/app/api/vault/retrieve/note/route.ts
@@ -1,21 +1,49 @@
 import { NextRequest, NextResponse } from "next/server";
 import { jwtDecrypt } from "jose";
 import { DecodeHex } from "@libs/enc-dec";
-import { DecryptAES } from "@libs/crypto";
+import { EncryptAES } from "@libs/crypto";
+import { GetPrismaClient } from "@libs/prisma";
 
-export async function GET(nextRequest: NextRequest) {
+interface VaultStoreNoteData {
+  title: string;
+  content: string;
+}
+
+export async function POST(nextRequest: NextRequest) {
   try {
     const encryptedJwt = nextRequest.cookies.get("encryptedjwt")?.value;
-    const vaultRetrieveNoteData = nextRequest.nextUrl.searchParams.get("note");
 
-    if (encryptedJwt !== undefined && vaultRetrieveNoteData !== null) {
+    if (encryptedJwt !== undefined) {
+      const vaultStoreNoteData: VaultStoreNoteData = await nextRequest.json();
+
       const { payload, protectedHeader } = await jwtDecrypt(encryptedJwt, DecodeHex(process.env.SECRET_KEY!), {
         issuer: "https://satupassword.com",
         audience: "https://satupassword.com",
       });
 
-      return NextResponse.json({ message: "Successful!", note: DecryptAES(vaultRetrieveNoteData, payload.masterKey as string) }, { status: 200 });
+      const login = await GetPrismaClient().login.findUniqueOrThrow({
+        where: {
+          email: payload.email as string,
+        },
+      });
+
+      const user = await GetPrismaClient().user.findUniqueOrThrow({
+        where: {
+          loginId: login.id,
+        },
+      });
+
+      await GetPrismaClient().note.create({
+        data: {
+          title: vaultStoreNoteData.title,
+          encryptedContent: EncryptAES(vaultStoreNoteData.content, payload.masterKey as string),
+          userId: user.id,
+        },
+      });
+
+      return NextResponse.json({ message: "Successful!" }, { status: 200 });
     }
+
     return NextResponse.json({ message: "Something went wrong!" }, { status: 400 });
   } catch {
     return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
--- src/app/api/vault/retrieve/password/route.ts
+++ src/app/api/vault/retrieve/password/route.ts
@@ -1,29 +1,56 @@
 import { NextRequest, NextResponse } from "next/server";
-import { PrismaClient } from "@prisma/client";
 import { jwtDecrypt } from "jose";
 import { DecodeHex } from "@libs/enc-dec";
-import { DecryptAES } from "@libs/crypto";
+import { EncryptAES } from "@libs/crypto";
+import { GetPrismaClient } from "@libs/prisma";
 
-export async function GET(nextRequest: NextRequest) {
+interface VaultStorePasswordData {
+  title: string;
+  url: string;
+  username: string;
+  password: string;
+}
+
+export async function POST(nextRequest: NextRequest) {
   try {
     const encryptedJwt = nextRequest.cookies.get("encryptedjwt")?.value;
-    const vaultRetrievePasswordData = nextRequest.nextUrl.searchParams.get("password");
 
-    if (encryptedJwt !== undefined && vaultRetrievePasswordData !== null) {
-      const prisma = new PrismaClient();
+    if (encryptedJwt !== undefined) {
+      const vaultStorePasswordData: VaultStorePasswordData = await nextRequest.json();
 
       const { payload, protectedHeader } = await jwtDecrypt(encryptedJwt, DecodeHex(process.env.SECRET_KEY!), {
         issuer: "https://satupassword.com",
         audience: "https://satupassword.com",
       });
 
-      return NextResponse.json(
-        { message: "Successful!", password: DecryptAES(vaultRetrievePasswordData, payload.masterKey as string) },
-        { status: 200 }
-      );
+      const login = await GetPrismaClient().login.findUniqueOrThrow({
+        where: {
+          email: payload.email as string,
+        },
+      });
+
+      const user = await GetPrismaClient().user.findUniqueOrThrow({
+        where: {
+          loginId: login.id,
+        },
+      });
+
+      await GetPrismaClient().password.create({
+        data: {
+          title: vaultStorePasswordData.title,
+          url: vaultStorePasswordData.url,
+          username: vaultStorePasswordData.username,
+          encryptedPassword: EncryptAES(vaultStorePasswordData.password, payload.masterKey as string),
+          userId: user.id,
+        },
+      });
+
+      return NextResponse.json({ message: "Successful!" }, { status: 200 });
     }
+
     return NextResponse.json({ message: "Something went wrong!" }, { status: 400 });
-  } catch {
+  } catch (exception) {
+    console.log(exception);
     return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
   }
 }
