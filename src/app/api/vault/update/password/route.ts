626ddd167a968c3cf45a6e9a227c916005e2fcdd
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2023-10-18 15:40:24 +0800

    Vault Update APIs.

---
+++ src/app/api/vault/update/password/route.ts
@@ -0,0 +1,60 @@
+import { NextRequest, NextResponse } from "next/server";
+import { jwtDecrypt } from "jose";
+import { DecodeHex } from "@libs/enc-dec";
+import { EncryptAES } from "@libs/crypto";
+import { GetPrismaClient } from "@libs/prisma";
+
+interface VaultUpdatePasswordData {
+  id: number;
+  title: string;
+  url: string;
+  username: string;
+  password: string;
+}
+
+export async function POST(nextRequest: NextRequest) {
+  try {
+    const encryptedJwt = nextRequest.cookies.get("encryptedjwt")?.value;
+
+    if (encryptedJwt !== undefined) {
+      const vaultUpdatePasswordData: VaultUpdatePasswordData = await nextRequest.json();
+
+      const { payload, protectedHeader } = await jwtDecrypt(encryptedJwt, DecodeHex(process.env.SECRET_KEY!), {
+        issuer: "https://satupassword.com",
+        audience: "https://satupassword.com",
+      });
+
+      const login = await GetPrismaClient().login.findUniqueOrThrow({
+        where: {
+          email: payload.email as string,
+        },
+      });
+
+      const user = await GetPrismaClient().user.findUniqueOrThrow({
+        where: {
+          loginId: login.id,
+        },
+      });
+
+      await GetPrismaClient().password.update({
+        where: {
+          id: vaultUpdatePasswordData.id,
+        },
+        data: {
+          title: vaultUpdatePasswordData.title,
+          url: vaultUpdatePasswordData.url,
+          username: vaultUpdatePasswordData.username,
+          encryptedPassword: EncryptAES(vaultUpdatePasswordData.password, payload.masterKey as string),
+          userId: user.id,
+        },
+      });
+
+      return NextResponse.json({ message: "Successful!" }, { status: 200 });
+    }
+
+    return NextResponse.json({ message: "Something went wrong!" }, { status: 400 });
+  } catch (exception) {
+    console.log(exception);
+    return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+  }
+}
