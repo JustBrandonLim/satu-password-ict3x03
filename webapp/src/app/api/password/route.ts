69f46f4fac433707b14e8f2ce9a7d5ed3e95ffcd
kevinthmm <kevinthmm@gmail.com>
2023-10-28 17:13:50 +0800

    modified the password API so that it reads user's query and returns a password based on what they requested

--- webapp/src/app/api/password/route.ts
+++ webapp/src/app/api/password/route.ts
@@ -1,9 +1,39 @@
-import { NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 import { GeneratePassword } from "@libs/password";
 
-export async function GET() {
+interface PasswordReqData {
+  uppercase: boolean,
+  lowercase: boolean,
+  numerical: boolean,
+  symbols: boolean,
+  passwordLength: number,
+}
+
+// export async function GET() {
+//   try {
+//     // Retrieve the request parameters
+//     return NextResponse.json({ message: "Successful!", password: GeneratePassword() }, { status: 200 });
+//   } catch {
+//     return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
+//   }
+// }
+
+export async function POST(nextRequest: NextRequest) {
   try {
-    return NextResponse.json({ message: "Successful!", password: GeneratePassword() }, { status: 200 });
+    // Retrieve the request parameters
+    const generatePasswordReqData: PasswordReqData = await nextRequest.json();
+    const uppercase = generatePasswordReqData.uppercase;
+    const lowercase = generatePasswordReqData.lowercase;
+    const numerical = generatePasswordReqData.numerical;
+    const symbols = generatePasswordReqData.symbols;
+    const passwordLength = generatePasswordReqData.passwordLength;
+
+    return NextResponse.json({ message: "Successful!", password: GeneratePassword(
+        uppercase,
+        lowercase,
+        numerical,
+        symbols,
+        passwordLength) }, { status: 200 });
   } catch {
     return NextResponse.json({ message: "Something went wrong!" }, { status: 500 });
   }
