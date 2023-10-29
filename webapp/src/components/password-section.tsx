b1706ba31ec9edbf217e2e3f88af97f409d8db4d
kevinthmm <kevinthmm@gmail.com>
2023-10-30 01:15:21 +0800

    fixed some validation warning with react componets on register and password components

--- webapp/src/components/password-section.tsx
+++ webapp/src/components/password-section.tsx
@@ -33,7 +33,7 @@ import { useFormContext } from "react-hook-form"
 // The actual component
 function PasswordSection(){
   // Retrieve all hook methods
-  const { register, setValue } = useFormContext()
+  const { setValue } = useFormContext()
   // Maintain Password Visibility State
   const [showPassword, setShowPassword] = React.useState(false)
   // Dialog open UseState
@@ -69,26 +69,25 @@ function PasswordSection(){
   return (
     <>
       {/* Password Field*/}
-      <FormField {...register("password")}
-        // name="password"
-        render={
-          ({ field }) => (
-          <FormItem>
-            <FormLabel>Password</FormLabel>
-            <FormControl>
-              <div className="relative">
-                <Input placeholder="Enter Password" type={showPassword?'text':'password'} {...field} onInput={handlePasswordChange}/>
-                <Button variant="ghost" type="button" size='icon' className="absolute right-0 bottom-0" aria-label="Toggle Passowrd Visibility" onClick={() => {setShowPassword(!showPassword)}}>
-                  <Eye className="absolute text-slate-400" visibility={showPassword? 'visible':'hidden'}/>
-                  <EyeOff className="absolute text-slate-300" visibility={showPassword? 'hidden':'visible'}/>
-                </Button>
-              </div>
-            </FormControl>
-            <FormMessage />
-          </FormItem>
-        )}
+      <FormField
+          name="password"
+          render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <div className="relative">
+                    <Input placeholder="Enter Password" type={showPassword?'text':'password'} {...field} onInput={handlePasswordChange}/>
+                    <Button variant="ghost" type="button" size='icon' className="absolute right-0 bottom-0" aria-label="Toggle Passowrd Visibility" onClick={() => {setShowPassword(!showPassword)}}>
+                      <Eye className="absolute text-slate-400" visibility={showPassword? 'visible':'hidden'}/>
+                      <EyeOff className="absolute text-slate-300" visibility={showPassword? 'hidden':'visible'}/>
+                    </Button>
+                  </div>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+          )}
       />
-      
+
       {/* Password Strength Checker*/}
       <div className="flex justify-center items-center space-x-4 my-2 mr-1">
         <label className="text-sm text-character-secondary">Strength</label>
@@ -128,13 +127,12 @@ function PasswordSection(){
       
       {/* Confirm Password Field*/}
       <FormField
-        {...register("confirmPassword")}
         name="confirmPassword"
         render={({ field }) => (
         <FormItem className="mt-4">
           <FormLabel>Confirm Password</FormLabel>
           <FormControl>
-            <Input placeholder="Re-enter Password" type="password" {...field} />
+            <Input placeholder="Re-enter Password" type="password" {...field} ref={null}/>
           </FormControl>
           <FormMessage />
         </FormItem>
