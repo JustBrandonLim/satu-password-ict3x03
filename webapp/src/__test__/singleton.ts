3aadaaa83333084c4473e5b688ca873e2c6ea1a0
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2023-11-04 17:24:49 +0800

    Update singleton.ts

--- webapp/src/__test__/singleton.ts
+++ webapp/src/__test__/singleton.ts
@@ -3,7 +3,7 @@ import { mockDeep, mockReset, DeepMockProxy } from "jest-mock-extended";
 
 import prisma from "@libs/prisma";
 
-jest.mock("@libs/prisma", () => ({
+jest.mock("../libs/prisma", () => ({
   __esModule: true,
   default: mockDeep<PrismaClient>(),
 }));
