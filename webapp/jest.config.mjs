7faf46914a3e089ef89e13f791ea93983ee43a78
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2023-11-05 06:15:27 +0800

    remove jest testing for prisma client

--- webapp/jest.config.mjs
+++ webapp/jest.config.mjs
@@ -11,9 +11,6 @@ const config = {
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
   testEnvironment: 'jest-environment-jsdom',
-  clearMocks: true,
-  preset: 'ts-jest',
-  setupFilesAfterEnv: ['<rootDir>/src/__test__/singleton.ts']
 }
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
