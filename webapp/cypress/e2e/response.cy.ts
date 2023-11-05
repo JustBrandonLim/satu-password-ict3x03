d9d9646765dd41fdb64ad9a8d0b7e5625d97ce6d
S0meDev99 <2101021@sit.singaporetech.edu.sg>
2023-11-05 15:20:02 +0800

    login and profile api test

---
+++ webapp/cypress/e2e/response.cy.ts
@@ -0,0 +1,48 @@
+import { authenticator } from "otplib";
+
+describe("/api/login", () => {
+  const totpSecret = "BUVBQAZZKFSCUHAL";
+  let cookieValues;
+
+  it("/api/login", () => {
+    let otpNow = authenticator.generate(totpSecret);
+
+    cy.request({
+      method: "POST",
+      url: "http://localhost:3000/api/login",
+      body: {
+        email: "alford@hentai.com",
+        password: "12345678",
+        otp: otpNow,
+      },
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+      cookieValues = response.headers["set-cookie"][0];
+
+      expect(response.status).to.equal(200);
+      expect(response.body).to.deep.equal({ message: "Successful!" });
+
+      // Store cookieValues in an alias
+      cy.wrap(cookieValues).as("cookieValues");
+    });
+  });
+
+  it("/api/profile", function () {
+    cy.request({
+      method: "GET",
+      url: "http://localhost:3000/api/profile",
+      headers: {
+        Cookie: this.cookieValues,
+      },
+    }).then((response) => {
+      cookieValues = response.headers["set-cookie"][0];
+
+      expect(response.status).to.equal(200);
+
+      // Store encryptedjwt in an alias
+      cy.wrap(cookieValues).as("cookieValues");
+    });
+  });
+});
